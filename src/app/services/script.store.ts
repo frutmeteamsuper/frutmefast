caff1170d5b373747f6cb73c4424b068046bb0c2
root <root@iPhone>
2022-06-13 21:34:03 -0400

    first commit

---
+++ src/app/services/script.store.ts
@@ -0,0 +1,17 @@
+
+ interface Scripts {
+    name: string;
+    src: string;
+}  
+export const ScriptStore: Scripts[] = [
+    {name: 'jquery', src: './assets/assets/js/jquery-3.6.0.min.js'},
+    {name: 'bootstrap.bundle', src: './assets/assets/js/bootstrap.bundle.min.js'},
+    {name: 'lord-icon', src: './assets/assets/js/lord-icon-2.1.0.js'},
+    {name: 'feather', src: './assets/assets/js/feather.min.js'},
+    {name: 'slick', src: './assets/assets/js/slick.js'},
+    {name: 'slick.min', src: './assets/assets/js/slick.min.js'},
+    {name: 'slick-custom', src: './assets/assets/js/slick-custom.js'},
+    {name: 'homescreen-popup', src: './assets/assets/js/homescreen-popup.js"'},
+    {name: 'theme-setting', src: './assets/assets/js/theme-setting.js'},
+    {name: 'script', src: './assets/assets/js/script.js'}
+];
